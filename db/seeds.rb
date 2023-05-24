f4132ab931fb8e637e3832d9ec9ac581d4fa3447
ali ouzidouh <ouzidouh.ali@gmail.com>
2023-05-24 15:41:30 +0200

    Exercice2 done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# db/seeds.rb
+
+require 'faker'
+require_relative '../app/models/restaurant'
+
+category_seed = ["chinese", "italian", "japanese", "french", "belgian"]
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts 'Creating 10 fake restaurants...'
+5.times do
+  restaurant = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.street_address, category: category_seed.sample )
+  restaurant.save!
+end
+puts "Finished!"
